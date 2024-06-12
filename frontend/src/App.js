ed28857df1a41f506515597bce7a5ee5794ce4e6
Adarsh Suman <84612800+adarsh3699@users.noreply.github.com>
2024-06-12 18:11:36 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import FootBar from './component/footer/FootBar';
 
 import './style/App.css';
 
-const apiBaseUrl = 'https://price-comparison-web.onrender.com/';
+const apiBaseUrl = 'https://comparison-api.bhemu.me/';
 // const apiBaseUrl = 'http://localhost:3000/';
 
 function App() {
